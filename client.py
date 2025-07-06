958ab7de79b6083c3bbe16db94a72501c4c98e9b
DefoNotKarma <utkarshg74@gmail.com>
2025-07-06 13:35:31 +0530

    send clients backlogs

--- client.py
+++ client.py
@@ -1,40 +1,110 @@
 import socket
 import threading
+import bz2
+import json
+from queue import Queue
 
-def find_open_port():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind(("", 0))
-        return s.getsockname()[1]
 
 FORMAT = 'utf-8'
-HEADER = 64
+HEADER = 8       # bytes
+LOG_HEADER = 64
 DISCONNECT_MESSAGE = "!dsc"
 PORT = 5050
 
-SERVER = "192.168.1.4"  # IP you want to connect
+SERVER = "192.168.1.6"  # IP you want to connect
 ADDR = (SERVER, PORT)
-
+BLANK_HEADER = str(len("")).encode(FORMAT)
+BLANK_HEADER += b' ' * (HEADER - len(BLANK_HEADER))
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
+user_input_queue = Queue()
+
+def get_user_input():
+    while True:
+        user_input = input()
+        user_input_queue.put(user_input)
+        print("\r")
+
+def recieve_messages():
+    while True:
+        message_size = client.recv(HEADER)
+        if message_size:
+            message_size = int(message_size.strip())
+            message = json.loads(client.recv(message_size).decode(FORMAT))
+
+            print(f"\r[{message['username']}] : {message["message"]}")
+
+            
 
 def send(message):
     message = message.encode(FORMAT)
-    msg_len_str = str(len(message)).encode('utf-8')
+    msg_len_str = str(len(message)).encode(FORMAT)
     msg_len_str += b' ' * (HEADER - len(msg_len_str))
     client.send(msg_len_str)
     client.send(message)
+    blank_header = str(0).encode(FORMAT).ljust(HEADER)
+    client.send(blank_header)  
+
+def handle_send_messages():
+    global connected
+    while connected:
+        if not user_input_queue.empty():
+            inp = user_input_queue.get()
+            send(inp)
+            print(f"[YOU] : {inp}")
+            if inp == DISCONNECT_MESSAGE:
+                connected = False
 
 connected = True
 
 inp = input("Enter Username : ")
 send(inp)
 
-while connected:
-    inp = input("[YOU] : ")
-    send(inp)
-    if inp == DISCONNECT_MESSAGE:
-        connected = False
+print(f"fetching logs...\n")
+
+logs_size = client.recv(LOG_HEADER)
+if logs_size:
+
+    logs_size = int(logs_size.strip())
+    logs = client.recv(logs_size)
+    logs = json.loads(bz2.decompress(logs))
+
+    if logs == []:
+        print("No logs to display.")
+    else:
+
+        for msg in logs:
+            print(f"[{msg['time']}] [{msg['username']}] : {msg['message']}")
+
+else:
+    print("couldn't fetch logs.")
+
+print("\n")
+
+get_input = threading.Thread(target=get_user_input, daemon=True).start()
+listener = threading.Thread(target=handle_send_messages, daemon=True)
+listener.start()
+
+try:
+
+    recieve_messages()
+
+except KeyboardInterrupt:
+    print("\nDisconnecting...")
+    connected = False
+    send(DISCONNECT_MESSAGE)
+
 
 print("\n------------You have exitted the chat successfully------------\n")
+
+
+
+
+
+
+
+
+
+# make recving in main and input taking as therad
--- server.py
+++ server.py
@@ -1,5 +1,10 @@
 import socket
 import threading
+from datetime import datetime
+import bz2
+import json
+from queue import Queue
+
 
 def find_open_port():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -14,14 +19,54 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ADDR = (SERVER, PORT)
 
 FORMAT = 'utf-8'
-HEADER = 64
+HEADER = 8
+LOG_HEADER = 64
 DISCONNECT_MESSAGE = "!dsc"
+BLANK_HEADER = str(len("")).encode(FORMAT)
+BLANK_HEADER += b' ' * (HEADER - len(BLANK_HEADER))
+
 
 server_socket.bind(ADDR)
 
+messages = []
+message_queue = Queue()
+thread_lock = threading.Lock()
+
 clients = []
 
+def send_chatlogs(conn, data):
+
+    data_size = str(len(data)).encode(FORMAT)
+    data_size += b' ' * (LOG_HEADER - len(data_size))
+    conn.send(data_size)
+    conn.send(data)
+
+def broardcast_new_messages():
+    while True:
+        with thread_lock:
+            addr, message = message_queue.get()
+
+        message = json.dumps(message).encode(FORMAT)
+
+        with thread_lock:
+            for client in list(clients):
+                try:
+                    if client.getpeername() == addr:
+                        continue
+                    
+                    data_size = str(len(message)).encode(FORMAT)
+                    data_size += b' ' * (HEADER - len(data_size))
+                    client.send(data_size)
+                    client.send(message)
+                except OSError:
+                    print("[ALERT] Found disconnected client. Removing them")
+                    clients.remove(client)
+
+            
+
+
 def handle_client(conn, addr):
+
     # getting username in same variable to save space
     username =  conn.recv(HEADER).decode(FORMAT)
     if username:
@@ -29,27 +74,51 @@ def handle_client(conn, addr):
         username = conn.recv(username).decode(FORMAT)
         if not username:
             username = addr
-    clients.append((username, addr))
-    print(clients)
-    print(f"[New Connection] : {addr} connected. Welcome {username}")
+
+
+    clients.append(conn) 
+
+    print(f"[New Connection] : {addr} connected. Welcome {username}.")
+
+    # send chat history
+    data = json.dumps(messages).encode(FORMAT)
+    data = bz2.compress(data)
+
+    send_chatlogs(conn, data)
+
+
+
     connected = True
 
     while connected:
-        get_user_msg_len = conn.recv(HEADER).decode(FORMAT)
+        try:
+            get_user_msg_len = conn.recv(HEADER).decode(FORMAT)
+
+            if get_user_msg_len:
+                get_user_msg_len = int(get_user_msg_len.strip())
+                message = conn.recv(get_user_msg_len).decode(FORMAT)
+
+                # maintain messages sent 
+                message_log = {"username" : username,
+                                "message" : message,
+                                "time" : datetime.now().strftime("%d/%m/%Y, %H:%M:%S")}
+                
+                messages.append(message_log)
+                message_queue.put((addr, message_log))
+                
+                if message == DISCONNECT_MESSAGE:
+                    connected = False
+                
+                print(f"[{username}]: {message}")
+                conn.recv(0)
+        except ConnectionResetError as err:
+            print(f"\n{username} severed the connection forcefully.")
+            connected = False
 
 
-        if get_user_msg_len:
-            get_user_msg_len = int(get_user_msg_len.strip())
-            message = conn.recv(get_user_msg_len).decode(FORMAT)
-            
-            if message == DISCONNECT_MESSAGE:
-                connected = False
-            
-            print(f"[{username}]: {message}")
-
-    print(f"\n{username} disconnected")
-
+    print(f"{username} disconnected.\n")
     conn.close()
+
 
 
 def start_server():
@@ -59,7 +128,11 @@ def start_server():
         conn, addr = server_socket.accept()
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
-        print(f"[Active Connections] : {threading.active_count() - 1}")
+        broadcaster = threading.Thread(target=broardcast_new_messages, daemon=True)
+        broadcaster.start()
+        print(f"[Active Threads] : {threading.active_count() - 1}")
+        print(f"[Active Connections] : {int((threading.active_count() - 1)/2)}")
+        
 
 print("STARTING SERVER...")
 start_server()
